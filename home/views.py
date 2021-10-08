4ffe633490ac417ec83b0e5f59d25b540e9696cf
riyadubeyy <rriiyyaa955@gmail.com>
2021-10-08 21:49:56 +0530

    netflix
    changes

--- home/views.py
+++ home/views.py
@@ -4,32 +4,16 @@ from typing import ContextManager
 from django.shortcuts import redirect, render
 from django.http import HttpResponse,HttpResponseRedirect, request
 
-
-
-
-
-
-
 # Create your views here.
-
-
 def netflix(request):
     return render (request,'netflix.html')
-
-
 
 def login(request):
      return render (request,'login.html')
 
-    
-   
-    
-
 def home(request):
      return render (request,'home.html')
   
-
-
 def about(request):
     return render (request,'about.html')
 
